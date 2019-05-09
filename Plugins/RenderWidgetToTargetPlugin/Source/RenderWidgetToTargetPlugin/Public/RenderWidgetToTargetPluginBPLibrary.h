a36650aa2ad826738bdf95112a333734098db575
Aquariaue4 <48570494+Aquariaue4@users.noreply.github.com>
2019-05-09 07:50:01 -0700

    NewProject

---
+++ Plugins/RenderWidgetToTargetPlugin/Source/RenderWidgetToTargetPlugin/Public/RenderWidgetToTargetPluginBPLibrary.h
@@ -0,0 +1,22 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Engine.h"
+
+#include "UMG.h"
+#include "SlateBasics.h"
+#include "SlateCore.h"
+#include "SWidget.h"
+#include "WidgetRenderer.h"
+
+#include "RenderWidgetToTargetPluginBPLibrary.generated.h"
+
+UCLASS()
+class URenderWidgetToTargetPluginBPLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_UCLASS_BODY()
+
+		UFUNCTION(Category = "RenderWidgetToTargetPlugin", BlueprintCallable)
+		static bool DrawWidgetToTarget(UTextureRenderTarget2D * Target, UUserWidget * WidgetToRender, FVector2D DrawSize, bool UseGamma, TextureFilter Filter, float DeltaTime);
+};
